9427e32671e0eb6be85836eebbc9eb474bc4129d
koturfilip@yahoo.com <koturfilip@yahoo.com>
2019-12-12 19:35:08 +0100

    Add project files.

---
+++ Sensio/Demo.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Demo.aspx.cs" Inherits="Sensio.Demo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        
+        <asp:Label ID="IdTekst" runat="server" Text="Unesi parametar/re"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        <p>
+            <asp:Button ID="IdSpremi" runat="server" Text="Spremi" OnClick="IdSpremi_Click" />
+            <asp:Button ID="IdPosalji" runat="server" Text="Pošalji" OnClick="IdPosalji_Click" />
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Tablica]"></asp:SqlDataSource>
+        </p>
+        <asp:Label ID="Label1" runat="server" Text="Status code:"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ Sensio/Demo.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Configuration;
+using System.Data.Sql;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net;
+
+namespace Sensio
+{
+    public partial class Demo : System.Web.UI.Page
+    {
+        
+        string tekst=ConfigurationManager.AppSettings.Get("Key0");
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database1.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+             
+        }
+
+        protected void IdSpremi_Click(object sender, EventArgs e)
+        {
+
+            connection.Open();
+            SqlCommand cmd = connection.CreateCommand();
+            cmd.CommandType=CommandType.Text;
+            cmd.CommandText = "insert into Tablica values('"+TextBox1.Text+"')";
+            cmd.ExecuteNonQuery();
+            connection.Close();
+
+        }
+
+        protected void IdPosalji_Click(object sender, EventArgs e)
+        {
+            tekst = tekst + TextBox1.Text;
+            tekst=tekst.Replace(' ', '&');
+            HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create(tekst);
+            HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+            TextBox2.Text=""+ (int)myHttpWebResponse.StatusCode;
+            myHttpWebResponse.Close();
+            
+        }
+    }
+}
